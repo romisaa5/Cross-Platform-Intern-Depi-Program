4994c5a65a84f33bf4eac291a0763ec7a159bfbf
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-07-21 05:47:10 +0200

    finish solve mcq

---
+++ week 4/Tasks/mcq.dart
@@ -0,0 +1,39 @@
+/*
+1. What is the purpose of an enum in Dart?
+A. To create multiple constructors
+B. To define a fixed set of constant values
+C. To handle exceptions
+D. To create dynamic lists
+Correct Answer: (B)
+
+2. What keyword is used to define an enum in Dart?
+A. class
+B. const
+C. enum
+D. final
+Correct Answer: (c)
+
+3. Which statement about try-catch in Dart is true?
+A. try can be used without catch or finally
+B. catch is used to define enums
+C. try must always be followed by throw
+D. try is only used for null safety
+Correct Answer: (A)
+
+4. What does the on keyword do in a try-catch block?
+A. Declares a variable
+B. Specifies the type of exception to catch
+C. Ends the program
+D. Loops through exceptions
+Correct Answer: (B)
+
+5. What is a custom exception used for?
+A. To override main()
+B. To create new keywords
+C. To handle application-specific errors
+D. To define default values
+Correct Answer: (C)
+
+
+*/
+
